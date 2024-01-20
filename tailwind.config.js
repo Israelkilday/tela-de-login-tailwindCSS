ba2644da00d1c5cf37e7a149d9aad561c85660c8
Israel Kilday <israel.kilday@yahoo.com.br>
2024-01-20 15:28:23 -0300

    test23

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./dist/**/*.{html,js}",
+    // "./dist/**/*.{html,js}",
+    "./dist/*.{html,js}",
   ],
   
   theme: {
